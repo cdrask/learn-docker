494b647b1bd30ea59369f8538d53ceac3714869a
cdrask <christianrask2@gmail.com>
2026-02-22 21:34:18 +0100

    Update

--- cmake/ewarm.cmake
+++ cmake/ewarm.cmake
@@ -1,13 +1,13 @@
-# Toolchain File for the IAR C/C++ Compiler (EW)
+# Toolchain File for the IAR C/C++ Compiler (CX)
 
 # Set CMake for cross-compiling
 set(CMAKE_SYSTEM_NAME Generic)
 
-# Set CMake to use the IAR C/C++ Compiler from the IAR Embedded Workbench for Arm
-# Update the paths if using any different supported target/version
-set(CMAKE_ASM_COMPILER "C:/iar/ewarm-9.70.2/arm/bin/iasmarm.exe")
-set(CMAKE_C_COMPILER   "C:/iar/ewarm-9.70.2/arm/bin/iccarm.exe")
-set(CMAKE_CXX_COMPILER "C:/iar/ewarm-9.70.2/arm/bin/iccarm.exe")
+# Set CMake to use the IAR C/C++ Compiler from the IAR Build Tools for Arm
+# Update if using a different supported target or operating system
+set(CMAKE_ASM_COMPILER /opt/iar/cxarm/arm/bin/iasmarm)
+set(CMAKE_C_COMPILER   /opt/iar/cxarm/arm/bin/iccarm)
+set(CMAKE_CXX_COMPILER /opt/iar/cxarm/arm/bin/iccarm)
 
 # Avoids running the linker during try_compile()
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
@@ -16,12 +16,12 @@ set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 # Reasonably recent IAR products ships with ninja (https://ninja-build.org)
 # The CMake code block below tries to find it. If not found, 
 # manually install the desired build system in your operating system
-# Alternatively: set(CMAKE_MAKE_PROGRAM "C:/path/to/ninja.exe")
+# Alternatively: set(CMAKE_MAKE_PROGRAM "/usr/bin/ninja")
 if(CMAKE_GENERATOR MATCHES "^Ninja.*$")
   find_program(CMAKE_MAKE_PROGRAM
     NAMES ninja
     PATHS $ENV{PATH}
-          "C:/iar/ewarm-9.70.2/common/bin"
+          /opt/iar/cxarm/common/bin
     REQUIRED)
 endif()
 
