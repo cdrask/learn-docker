58323487aa52cb071b409bb147a0ce283b95336a
cdrask <christianrask2@gmail.com>
2026-02-22 21:40:53 +0100

    Update

--- cmake/ewarm.cmake
+++ cmake/ewarm.cmake
@@ -5,25 +5,12 @@ set(CMAKE_SYSTEM_NAME Generic)
 
 # Set CMake to use the IAR C/C++ Compiler from the IAR Build Tools for Arm
 # Update if using a different supported target or operating system
-set(CMAKE_ASM_COMPILER /opt/iar/cxarm/arm/bin/iasmarm)
-set(CMAKE_C_COMPILER   /opt/iar/cxarm/arm/bin/iccarm)
-set(CMAKE_CXX_COMPILER /opt/iar/cxarm/arm/bin/iccarm)
+set(CMAKE_ASM_COMPILER iasmarm)
+set(CMAKE_C_COMPILER   iccarm)
+set(CMAKE_CXX_COMPILER iccarm)
 
 # Avoids running the linker during try_compile()
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
-
-# Set the default build tool for Ninja gnerators
-# Reasonably recent IAR products ships with ninja (https://ninja-build.org)
-# The CMake code block below tries to find it. If not found, 
-# manually install the desired build system in your operating system
-# Alternatively: set(CMAKE_MAKE_PROGRAM "/usr/bin/ninja")
-if(CMAKE_GENERATOR MATCHES "^Ninja.*$")
-  find_program(CMAKE_MAKE_PROGRAM
-    NAMES ninja
-    PATHS $ENV{PATH}
-          /opt/iar/cxarm/common/bin
-    REQUIRED)
-endif()
 
 # Apply compiler options globally (was target_compile_options on `tutorial`)
 add_compile_options(
