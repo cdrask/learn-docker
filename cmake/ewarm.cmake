6b15156095761b758d8b181f23a0a24e1ff11431
cdrask <christianrask2@gmail.com>
2026-02-22 21:42:03 +0100

    Update

--- cmake/ewarm.cmake
+++ cmake/ewarm.cmake
@@ -3,12 +3,6 @@
 # Set CMake for cross-compiling
 set(CMAKE_SYSTEM_NAME Generic)
 
-# Set CMake to use the IAR C/C++ Compiler from the IAR Build Tools for Arm
-# Update if using a different supported target or operating system
-set(CMAKE_ASM_COMPILER iasmarm)
-set(CMAKE_C_COMPILER   iccarm)
-set(CMAKE_CXX_COMPILER iccarm)
-
 # Avoids running the linker during try_compile()
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
